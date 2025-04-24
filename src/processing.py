f3be6aaf66782401710d1338b8d3f65607b28137
VoronkoV_Y <voronkov-y@yandex.ru>
2025-04-24 09:51:20 -0700

    add func sort_by_date in processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,12 @@
-def filter_by_state(user_info_list: list[dict], state="EXECUTED": str) -> list[dict]:
+def filter_by_state(user_info_list: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """Функция фильтрует список словарей по ключу \'state\'"""
     filtered_user_info = []
     for user_info in user_info_list:
         if user_info["state"] == state:
             filtered_user_info.append(user_info)
     return filtered_user_info
+
+
+def sort_by_date(user_info_list: list[dict], reverse_param: bool = True) -> list[dict]:
+    sorted_user_info_list = sorted(user_info_list, key=lambda x: x["date"], reverse=reverse_param)
+    return sorted_user_info_list
