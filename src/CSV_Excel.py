2d95aa9c2fac6a167424575f7fdf5e1f873c861f
VoronkoV_Y <voronkov-y@yandex.ru>
2025-07-30 01:43:03 -0700

    add csv_reader_fnc & excel_reader_fnc in CSV_Excel.py

--- src/CSV_Excel.py
+++ src/CSV_Excel.py
@@ -4,10 +4,22 @@ import csv
 
 def csv_reader_fnc(path_csv: str) -> list[dict]:
     """Функция для считывания финансовых операций из CSV - выдает список словарей с транзакциями"""
-    data = []
-    with open(path_csv) as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            data.append(row)
-    return data
+    try:
+        data = pd.read_csv(path_csv).to_dict(orient='records')
+        return data
+    except FileNotFoundError:
+        return (f"Ошибка! Файл {path_csv} не найден!")
 
+
+def excel_reader_fnc(path_excel: str) -> list[dict]:
+    """Функция для считывания финансовых операций из Excel - выдает список словарей с транзакциями."""
+    try:
+        data = pd.read_excel(path_excel).to_dict(orient='records')
+        return data
+    except FileNotFoundError:
+        return (f"Ошибка! Файл {path_excel} не найден!")
+
+
+if __name__ == "__main__":
+    # print(csv_reader_fnc("data/transactions.csv"))
+    print(excel_reader_fnc("data/transactions_excel.xlsx"))
